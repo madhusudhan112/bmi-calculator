b0c8f1875a12b15df31abea30504d0b5ce06b3d4
madhusudhan112 <madhusudhan.ghimire@asparksys.com>
2022-01-19 12:59:06 +0545

    Project Created

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import './screens/app_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BmiCalculator());
+}
+
+class BmiCalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Bmi Calculator",
+      theme: ThemeData(
+        primaryColor: Colors.grey[400],
+        backgroundColor: Colors.white,
+      ),
+      home : AppHome(),
+    );
+  }
+}
---
+++ lib/screens/app_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppHome extends StatefulWidget {
+ @override
+ State<AppHome> createState() => _AppHome();
+}
+
+class _AppHome extends State<AppHome> {
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
