b5e94ddc220b8ef5f0ac4cb7e72d1a737e2e23c0
madhusudhan112 <madhusudhan.ghimire@asparksys.com>
2022-01-26 13:38:26 +0545

    fix:validation, width

--- lib/calculator/body_fat_calculator.dart
+++ lib/calculator/body_fat_calculator.dart
@@ -14,11 +14,17 @@ class BodyFatCalculator {
   }
 
   String result() {
-    return "Your Body Fat Percentage is " + bf!.toStringAsFixed(1);
+    if (bf!.isNegative) {
+      return "You might have given invalid values, Body Fat Cant be Negative";
+    } else {
+      return "Your Body Fat Percentage is " + bf!.toStringAsFixed(1);
+    }
   }
 
   String result2() {
-    if (bf! <= 8) {
+    if (bf!.isNegative) {
+      return "Body Fat cant be Negative";
+    } else if (bf! <= 8) {
       return "You have much lower body fat take a good care or consult your doctor";
     } else if (bf! >= 30) {
       return "You have higher body fat please take a good care or consult your doctor";
